5cf8f7cda4765d4c26011b7d6ee73cad8c03359f
Kesha <kesha.marges@gmail.com>
2022-12-02 04:31:35 -0500

    commit tree c952e31a005acd3dcc4828a0143ade777321e477

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+const num1 = 2;
+const num2 = 31;
+const multiply = num1*num2
 
-// Write your code here
+const random = Math.ceil(Math.random());
+
+const num3 = 16;
+const num4 = 4;
+const mod = num3/num4;
+
+
+const max = Math.max(5,10,15,20);
